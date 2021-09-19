9251216cd1d5f0abe4a611120fbd0d85feb3b95d
unknown <caitlin.estrellita@gmail.com>
2021-09-19 00:45:49 -0700

    responsive

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,13 +1,15 @@
 import React from "react"
 import TopBarLinks from "./TopBarLinks"
 import {Link} from 'react-router-dom'
+import Burger from './Burger'
 
 const TopBar = () => {
   return (
     <div className="topBar">
       <div className="topBarWrapper">
         <Link to="/" className="logo"> 5 Rings Pictures </Link>
-        <TopBarLinks  />
+        <Burger />
+        
       </div>
     </div>
   )
--- src/components/TopBarLinks.js
+++ src/components/TopBarLinks.js
@@ -1,14 +1,35 @@
 import React from "react"
 import TopBarLink from "./TopBarLink"
+import styled from 'styled-components'
 
-const TopBarLinks = () => {
+const StyledWrapper = styled.div `
+display: flex;
+flex-flow: row nowrap;
+
+  @media (max-width: 1080px) {
+    flex-flow: column nowrap;
+    background-color: rgb(39, 39, 39);
+    padding-right: 20px;
+    position: fixed;
+    top: 0;
+    right: 0;
+    height: 100vh;
+    width: 300x;
+    padding-top: 100px;
+    transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
+    transition: transform 0.3s ease-in-out;
+  }
+  `
+
+const TopBarLinks = ({open}) => {
+
   return (
-    <div className="topBarLinks">
+    <StyledWrapper open={open}>
       <TopBarLink name="About"/>
       <TopBarLink name="Portfolio"/>
       <TopBarLink name="Blog"/>
       <TopBarLink name="Contact"/>
-    </div>
+    </StyledWrapper>
   )
 }
 
