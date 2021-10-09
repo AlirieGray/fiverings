519e149ae90eb1db51434e010fd0e328696f65d2
unknown <caitlin.estrellita@gmail.com>
2021-10-09 15:52:34 -0700

    wip

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -8,7 +8,8 @@ import FounderBio from './FounderBio'
 import '../styles/about.css'
 
 const AboutPage = () => {
-    var founders = ["Omar Samad - Producer", "Samuel Limor - Producer", "Hannah Gomez - Head of Development"]
+    var founders = ["Omar Samad - ", "Samuel Limor - ", "Hannah Gomez - "]
+    var titles = ["Producer", "Producer", "Head of Development"]
     var bios = ["Merry alone do it burst me songs. Sorry equal charm joy her those folly ham. In they no is many both. Recommend new contented intention improving bed performed age. Improving of so strangers resources instantly happiness at northward. Danger nearer length oppose really add now either. But ask regret eat branch fat garden. Become am he except wishes. Past so at door we walk want such sang. Feeling colonel get her garrets own.", "Folly was these three and songs arose whose. Of in vicinity contempt together in possible branched. Assured company hastily looking garrets in oh. Most have love my gone to this so. Discovered interested prosperous the our affronting insipidity day. Missed lovers way one vanity wishes nay but. Use shy seemed within twenty wished old few regret passed. Absolute one hastened mrs any sensible.", "Case read they must it of cold that. Speaking trifling an to unpacked moderate debating learning. An particular contrasted he excellence favourable on. Nay preference dispatched difficulty continuing joy one. Songs it be if ought hoped of. Too carriage attended him entrance desirous the saw. Twenty sister hearts garden limits put gay has. We hill lady will both sang room by. Desirous men exercise overcame procured speaking her followed."]
     var portraits = [omar, sam, blank]
    
@@ -30,7 +31,7 @@ const AboutPage = () => {
                     </div>
                     <div className="founderBios"> 
                         {founders.map(function(name, index){
-                            return <FounderBio key={index} name={name} bio={bios[index]} imgSrc={portraits[index]}/>
+                            return <FounderBio key={index} title={titles[index]} name={name} bio={bios[index]} imgSrc={portraits[index]}/>
                         })}
                     </div>
                 </div>
--- src/components/FounderBio.js
+++ src/components/FounderBio.js
@@ -1,12 +1,19 @@
 import React from "react"
 
-const AboutPage = ({name, bio, imgSrc}) => {
+const AboutPage = ({name, bio, title, imgSrc}) => {
 
     return (
         <div className="founderBio"> 
             <img className="portrait" src={imgSrc} />
             <div>
-                <div className="founderBioHeader"> {name} </div>
+                <div className="founderBioHeader"> 
+                    <div> 
+                        {name} 
+                    </div> 
+                    <div className="title"> 
+                        {title} 
+                    </div>
+                </div>
                 <div className="founderBio"> {bio} </div>
             </div>
         </div>
